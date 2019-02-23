71644e35f7f26fc7bc5bd3281f6043565d2fc0a3
Pi <pi@pi.com>
2019-02-23 21:50:42 +0000

    Refactored S3 client setup to use credentials from JSON

--- upload.py
+++ upload.py
@@ -5,6 +5,8 @@ import time
 #from boto.s3.connection import S3Connection
 #from boto.s3.key import Key
 import boto3
+from datetime import datetime
+import json
 
 AWS_ACCESS =''
 AWS_SECRET= ''
@@ -23,18 +25,53 @@ def upload_S3 (dir, file):
     k.key=file
     k.set_contents_from_filename(dir+file)
 
+sleep_timeout = 3
+
+def setup_aws():
+    with open('credentials.json') as json_file:
+        json_data = json.loads(json_file.read())
+        access_key = json_data['AWS_ACCESS']
+        secret = json_data['AWS_SECRET']
+
+        s3=boto3.client('s3',
+            aws_access_key_id = access_key,
+            aws_secret_access_key = secret,
+            region_name = 'eu-west-2'
+        )
+
+        return s3
+
+
 if __name__ == "__main__":
-    ap = ArgumentParser()
-    ap.add_argument('output')
+    s3 = setup_aws()
 
-    args = ap.parse_args()
+    if not s3:
+        print("S3 failed to configure")
+        exit(-1)
+
+    print("S3 setup correctly")
+
     with PiCamera() as camera:
+        print("camera initialised")
         camera.resolution = 1024, 768
+
         camera.start_preview()
+        for _ in range(5):
+            now = datetime.now()
+            now_str = now.strftime('%m_%d_%y_%H_%M_%S')
 
-        time.sleep(2)
+            # prefix filename with now stirng
+            filename = now_str + ".jpg"
 
-        camera.capture(args.output)
-        s3.upload_file(os.path.join(directory,args.output), bucket_name, args.output)
+            camera.capture(filename)
+            s3.upload_file(os.path.join(directory, filename), bucket_name, filename)
+
+            print("Uploaded %s successfully" % filename)
+
+            time.sleep(sleep_timeout)
+
         #response=s3.list_buckets()
         #print(response)
+
+        camera.stop_preview()
+        camera.close()
